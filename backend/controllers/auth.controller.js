fb27f72c3ce69f0d035b6d7c3e9d2f7bc0e367b3
Mohamed0Hisham <hishamsovereign321@gmail.com>
2024-12-21 16:17:47 +0200

    finished todo

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,1 +1,44 @@
-export const signin = async (req, res) => {};
+import argon from "argon2";
+import { UsersRepo } from "../repositories/user.repo.js";
+import { generateToken } from "../helpers/generateOTP.js";
+
+const userRepo = new UsersRepo();
+export const signin = async (req, res) => {
+	const { email, password } = req?.body;
+	if (email == null || password == null) {
+		return res.status(400).json({
+			message: "Email and password are required",
+		});
+	}
+	try {
+		const user = await userRepo.getUsers({ email });
+
+		if (user.length == 0) {
+			return res.status(404).json({ message: "invalid email provided" });
+		}
+		if (email !== user[0].email) {
+			return res
+				.status(401)
+				.json({ message: "invalid email or password" });
+		}
+		//checking password
+		const isCorrectPassword = await argon.verify(
+			user[0].password,
+			password
+		);
+		if (isCorrectPassword === false) {
+			return res
+				.status(401)
+				.json({ message: "invalid email or password" });
+		}
+		const token = generateToken();
+		return res
+			.status(200)
+			.json({ message: "successfully signed in", token });
+	} catch (error) {
+		console.log(error);
+		return res
+			.status(500)
+			.json({ message: "internal server error while signing in" });
+	}
+};
--- backend/helpers/generateOTP.js
+++ backend/helpers/generateOTP.js
@@ -1,4 +1,32 @@
+const smalls = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
+const capitals = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+];
+
+const specials = [
+    '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', 
+    '[', ']', '{', '}', ';', ':', '"', "'", '<', '>', ',', '.', '?', '/'
+];
 export default function generateID() {
-	const id = `${Math.floor(1000000000 + Math.random() * 9000000000)}`;
-	return id;
+    const id = `${Math.floor(1000000000 + Math.random() * 9000000000)}`;
+    return id;
+}
+
+export function generateToken() {
+	let token = "";
+	for (let index = 0; index < 64; index++) {
+		const whichArr = Math.floor(1 + Math.random() * 3);
+		if (whichArr === 1) {
+			token += smalls[Math.floor(Math.random() * 26)];
+		} else if (whichArr === 2) {
+			token += capitals[Math.floor(Math.random() * 26)];
+		} else if (whichArr === 3) {
+			token += specials[Math.floor(Math.random() * specials.length)];
+		}
+	}
+	return token;
 }
