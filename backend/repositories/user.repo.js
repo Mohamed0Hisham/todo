6b9369e101757109bd42326ed5d58244a25ab72c
Mohamed0Hisham <hishamsovereign321@gmail.com>
2024-12-21 16:29:01 +0200

    finished todo some enhancments

--- backend/repositories/user.repo.js
+++ backend/repositories/user.repo.js
@@ -2,37 +2,68 @@ import { connectToDB } from "../config/db.connect.js";
 import { ObjectId } from "mongodb";
 
 export class UsersRepo {
-	constructor() {
-		this.collectionName = "users";
-	}
-	async getCollection() {
-		const db = await connectToDB();
-		return db.collection(this.collectionName);
-	}
-	async getUsers(filters = {}) {
-		const collection = await this.getCollection();
-		return collection.find(filters).toArray();
-	}
-	async getUserByID(id) {
-		const collection = await this.getCollection();
-		const objectId = new ObjectId(id);
-		return collection.findOne({ _id: objectId });
-	}
-	async createUser(user) {
-		const collection = await this.getCollection();
-		return collection.insertOne(user);
-	}
-	async updateUser(id, update) {
-		const collection = await this.getCollection();
-		const objectId = new ObjectId(id);
-		return collection.updateOne(
-			{ _id: objectId },
-			{ $set: update },
-		);
-	}
-	async deleteUser(id) {
-		const collection = await this.getCollection();
-		const objectId = new ObjectId(id);
-		return collection.deleteOne({ _id: objectId });
-	}
+    constructor() {
+        this.collectionName = "users";
+    }
+
+    async getCollection() {
+        const db = await connectToDB();
+        return db.collection(this.collectionName);
+    }
+
+    async getUsers(filters = {}) {
+        try {
+            const collection = await this.getCollection();
+            return await collection.find(filters).toArray();
+        } catch (error) {
+            console.error('Error fetching users:', error);
+            throw new Error('Database error');
+        }
+    }
+
+    async getUserByID(id) {
+        try {
+            const collection = await this.getCollection();
+            const objectId = new ObjectId(id);
+            return await collection.findOne({ _id: objectId });
+        } catch (error) {
+            console.error('Error fetching user by ID:', error);
+            throw new Error('Database error');
+        }
+    }
+
+    async createUser(user) {
+        try {
+            const collection = await this.getCollection();
+            return await collection.insertOne(user);
+        } catch (error) {
+            console.error('Error creating user:', error);
+            throw new Error('Database error');
+        }
+    }
+
+    async updateUser(id, update) {
+        try {
+            const collection = await this.getCollection();
+            const objectId = new ObjectId(id);
+            return await collection.updateOne(
+                { _id: objectId },
+                { $set: update },
+            );
+        } catch (error) {
+            console.error('Error updating user:', error);
+            throw new Error('Database error');
+        }
+    }
+
+    async deleteUser(id) {
+        try {
+            const collection = await this.getCollection();
+            const objectId = new ObjectId(id);
+            return await collection.deleteOne({ _id: objectId });
+        } catch (error) {
+            console.error('Error deleting user:', error);
+            throw new Error('Database error');
+        }
+    }
 }
